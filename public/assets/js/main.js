20183d2cd413d28863fe125f43e3b49a9f610d01
cem <cersozz@gmail.com>
2019-02-14 19:27:50 -0500

    drawing charts

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,159 +1,58 @@
 
-$(document).ready(function() {
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyA0EPA1Gm0IHCFRhy3z2YG2hERSby8hiuU",
+	authDomain: "muzoiotclassroom.firebaseapp.com",
+	databaseURL: "https://muzoiotclassroom.firebaseio.com",
+	projectId: "muzoiotclassroom",
+	storageBucket: "muzoiotclassroom.appspot.com",
+	messagingSenderId: "633687263683"
+};
+firebase.initializeApp(config);
 
-	// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyA0EPA1Gm0IHCFRhy3z2YG2hERSby8hiuU",
-		authDomain: "muzoiotclassroom.firebaseapp.com",
-		databaseURL: "https://muzoiotclassroom.firebaseio.com",
-		projectId: "muzoiotclassroom",
-		storageBucket: "muzoiotclassroom.appspot.com",
-		messagingSenderId: "633687263683"
-	};
-	firebase.initializeApp(config);
+let temp_data = {'type': 'line',
+                 'axisYType': "primary",
+                 'name': "Temperature",
+                 'showInLegend': false,
+                 'markerSize': 5,
+				 'dataPoints': []}
 
-	// Create reference
-	var dbRefObject = firebase.database().ref().child('hisarcs').child('data');
+let pressure_data = {'type': 'line',
+                    'axisYType': "secondary",
+                    'name': "Pressure",
+                    'showInLegend': true,
+                    'markerSize': 0,
+					'dataPoints': []}
 
-	// Sync object changes
-	dbRefObject.on('value', snap => {
-		var snapVal = snap.val();
-		var tempDATA = [];
-		// Table ##############################################
-		var key = Object.keys(snapVal);
-		for(var value in snapVal) {
-			var temperature = snapVal[value]['temp'];
-			var pressure = snapVal[value]['pres'];
-			var ts = snapVal[value]['ts'].substring(11,19).substring(11,13);
 
-			tempDATA.push([parseInt(ts), temperature]);
+function toogleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	} else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
 
-			var trElement = document.createElement("tr");
-			var temperatureTr = document.createElement("th");
-			var pressureTr = document.createElement("th");
-			var tsTr = document.createElement("th");
+var temp_chart = new CanvasJS.Chart("temp-graph", { 
+	title: {text: "Temperature"},
+	animationEnabled: true,
+	zoomEnabled: true,
+	axisX: {valueFormatString: "DD MMM HH:MM"},
+	axisY: {title: "Degrees (C)"},
+	axisY2: {title: "Pressure (PSI)"},
+	toolTip: {shared: true},
+	legend: {cursor: "pointer", verticalAlign:"top", horizontalAlign: "center", dockInsidePlotArea: true, itemclick: toogleDataSeries},
+	data: [temp_data, pressure_data]
+});
 
-			temperatureTr.innerHTML = temperature;
-			pressureTr.innerHTML = pressure;
-			tsTr.innerHTML = ts;
+// Create reference
+var data_ref = firebase.database().ref().child('hisarcs').child('data');
 
-			trElement.appendChild(temperatureTr);
-			trElement.appendChild(pressureTr);
-			trElement.appendChild(tsTr);
-			document.getElementById("table").appendChild(trElement);
-		}
-
-		// Graph Data ##############################################
-		var graphData = [{
-				// Visits
-				data: [[6, 1300], [7, 1600], [8, 1900], [9, 2100], [10, 2500], [11, 2200], [12, 2000], [13, 1950], [14, 1900], [15, 2000]],
-				color: '#71c73e'
-			}, 
-			{
-				// Returning Visits
-				data: [ [6, 500], [7, 600], [8, 550], [9, 600], [10, 800], [11, 900], [12, 800], [13, 850], [14, 830], [15, 1000] ],
-				color: '#77b7c5',
-				points: { radius: 4, fillColor: '#77b7c5' }
-			}
-		];
-
-		// Lines Graph #############################################
-		$.plot($('#graph-lines'), graphData, {
-			series: {
-				points: {
-					show: true,
-					radius: 5
-				},
-				lines: {
-					show: true
-				},
-				shadowSize: 0
-			},
-			grid: {
-				color: '#646464',
-				borderColor: 'transparent',
-				borderWidth: 20,
-				hoverable: true
-			},
-			xaxis: {
-				tickColor: 'transparent',
-				tickDecimals: 2
-			},
-			yaxis: {
-				tickSize: 1000
-			}
-		});
-
-		// Bars Graph ##############################################
-		$.plot($('#graph-bars'), graphData, {
-			series: {
-				bars: {
-					show: true,
-					barWidth: .9,
-					align: 'center'
-				},
-				shadowSize: 0
-			},
-			grid: {
-				color: '#646464',
-				borderColor: 'transparent',
-				borderWidth: 20,
-				hoverable: true
-			},
-			xaxis: {
-				tickColor: 'transparent',
-				tickDecimals: 2
-			},
-			yaxis: {
-				tickSize: 1000
-			}
-		});
-
-		// Graph Toggle ############################################
-		$('#graph-bars').hide();
-
-		$('#lines').on('click', function (e) {
-			$('#bars').removeClass('active');
-			$('#graph-bars').fadeOut();
-			$(this).addClass('active');
-			$('#graph-lines').fadeIn();
-			e.preventDefault();
-		});
-
-		$('#bars').on('click', function (e) {
-			$('#lines').removeClass('active');
-			$('#graph-lines').fadeOut();
-			$(this).addClass('active');
-			$('#graph-bars').fadeIn().removeClass('hidden');
-			e.preventDefault();
-		});
-
-		// Tooltip #################################################
-		function showTooltip(x, y, contents) {
-			$('<div id="tooltip">' + contents + '</div>').css({
-				top: y - 16,
-				left: x + 20
-			}).appendTo('body').fadeIn();
-		}
-
-		var previousPoint = null;
-
-		$('#graph-lines, #graph-bars').bind('plothover', function (event, pos, item) {
-			if (item) {
-				if (previousPoint != item.dataIndex) {
-					previousPoint = item.dataIndex;
-					$('#tooltip').remove();
-					var x = item.datapoint[0],
-						y = item.datapoint[1];
-						showTooltip(item.pageX, item.pageY, y + ' visitors at ' + x + '.00h');
-				}
-			} else {
-				$('#tooltip').remove();
-				previousPoint = null;
-			}
-		});
-
-	});
-
-}());
-
+data_ref1.on("child_added", (snap, prev)=>{
+	var new_data = snap.val();
+	date = new Date(new_data.ts)
+	pressure_data.dataPoints.push({'y': parseInt(new_data.pres), 'x': date})
+	temp_data.dataPoints.push({y: parseInt(new_data.temp), x: date})
+	temp_chart.render();
+});
